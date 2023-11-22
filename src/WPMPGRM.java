a388cc448189f39548dd5df38996be8f1fdecdb3
MJ <mjsince2022@gmail.com>
2023-11-22 12:25:08 +0530

    first intellij commit

---
+++ src/WPMPGRM.java
@@ -0,0 +1,39 @@
+import java.time.LocalTime;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class WPMPGRM {
+     static String[] words={"The","quick","brown","fox","jumps","over","the","lazy","dog"};
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("3");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("2");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("1");
+        TimeUnit.SECONDS.sleep(1);
+
+        Random rand=new Random();
+        for (int i=0;i<10;i++) {
+            System.out.print(words[rand.nextInt(9)]+" ");
+        }
+        System.out.println();
+
+        double start=LocalTime.now().toNanoOfDay();
+
+        Scanner scan=new Scanner(System.in);
+
+        String typedwords= scan.nextLine();
+        double end=LocalTime.now().toNanoOfDay();
+        double elapsedTime=end-start;
+        double seconds=elapsedTime/1000000000.0;
+        int numChars=typedwords.length();
+        int wpm=(int)((((double)numChars / 5)/seconds)*60);
+        System.out.println("your WPM is "+wpm+"!");
+
+        System.out.println(seconds);
+        System.out.println(typedwords);
+
+
+    }
+}
